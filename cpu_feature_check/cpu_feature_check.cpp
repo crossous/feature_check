4e36d7e5f10943422a9bd2ed667543e688eab35b
crossous <870671204@qq.com>
2024-10-12 15:20:46 +0800

    init commit

---
+++ cpu_feature_check/cpu_feature_check.cpp
@@ -0,0 +1,176 @@
+﻿#include <iostream>
+
+#include <cstdint>
+
+#define CPUID_FEATURES_SSE2 (1 << 26)
+
+#ifdef _MSC_VER
+#define COMPILER_MSVC 1
+#elif defined(__clang__)
+#define COMPILER_CLANG 1
+#elif defined(__GNUC__)
+#define COMPILER_GCC 1
+#endif
+
+#ifdef __EMSCRIPTEN__
+
+void __cpuid(int data[4], int function_id)
+{
+    data[0] = data[1] = data[2] = data[3] = 0;
+    if (function_id == 0)
+    {
+        data[0] = 1; // Basic info supported
+        return;
+    }
+    else if (function_id == 1)
+    {
+        // Mimic fields reported by x86 cpuid instruction:
+        // https://learn.microsoft.com/en-us/cpp/intrinsics/cpuid-cpuidex?view=msvc-170
+#ifdef __SSE__
+        data[3] |= 1 << 25;
+#endif
+#ifdef __SSE2__
+        data[3] |= 1 << 26;
+#endif
+#ifdef __SSE3__
+        data[2] |= 1;
+#endif
+#ifdef __SSSE3__
+        data[2] |= 1 << 9;
+#endif
+#ifdef __SSE_4_1__
+        data[2] |= 1 << 19;
+#endif
+#ifdef __SSE_4_2__
+        data[2] |= 1 << 20;
+#endif
+    }
+}
+
+#elif COMPILER_MSVC
+
+// define __cpuid intrinsic
+#include <intrin.h>
+
+#elif COMPILER_CLANG || COMPILER_GCC
+
+#if defined(__x86_64__)
+#   define __cpuid(array, func) \
+    { \
+        __asm__ __volatile__("pushq %%rbx     \n\t" /* save %rbx */ \
+                             "cpuid            \n\t" \
+                             "popq %%rbx \n\t" /* restore the old %rbx */ \
+                             : "=a"(array[0]), "=D"(array[1]), "=c"(array[2]), "=d"(array[3]) \
+                             : "a"(func) \
+                             : "cc");\
+    }
+#else
+#   define __cpuid(array, func) \
+    { \
+        __asm__ __volatile__("xchg %%ebx, %%edi      \n\t" /* save %ebx */ \
+                            "cpuid            \n\t" \
+                            "xchg %%ebx, %%edi   \n\t" /* restore the old %ebx */ \
+                            : "=a"(array[0]), "=D"(array[1]), "=c"(array[2]), "=d"(array[3]) \
+                            : "a"(func) \
+                            : "cc");\
+    }
+#endif //defined(__x86_64__)
+
+#else
+#define __cpuid(a, b)
+#endif
+
+static inline uint64_t xgetbv_impl()
+{
+#   ifdef __EMSCRIPTEN__
+    return 0;
+#   elif COMPILER_CLANG || COMPILER_GCC
+    uint32_t eax, edx;
+
+    __asm __volatile(
+    ".byte 0x0f, 0x01, 0xd0" // xgetbv instruction isn't supported by some older assemblers, so just emit it raw
+        : "=a" (eax), "=d" (edx) : "c" (0)
+        );
+
+    return ((UInt64)edx << 32) | eax;
+#   elif COMPILER_MSVC
+    return _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+#   else
+    return 0;
+#   endif
+}
+
+static void cpuid_ex_impl(uint32_t eax, uint32_t ecx, uint32_t* abcd)
+{
+#ifdef __EMSCRIPTEN__
+    abcd[0] = 0; abcd[1] = 0; abcd[2] = 0; abcd[3] = 0;
+#elif COMPILER_MSVC
+
+    __cpuidex((int*)abcd, eax, ecx);
+
+#else
+    uint32_t ebx = 0, edx = 0;
+#if defined(__i386__) && defined(__PIC__)
+    // for PIC under 32 bit: EBX can't be modified
+    __asm__("movl %%ebx, %%edi \n\t cpuid \n\t xchgl %%ebx, %%edi" : "=D" (ebx), "+a" (eax), "+c" (ecx), "=d" (edx));
+#else
+    __asm__("cpuid" : "+b" (ebx), "+a" (eax), "+c" (ecx), "=d" (edx));
+#endif
+    abcd[0] = eax; abcd[1] = ebx; abcd[2] = ecx; abcd[3] = edx;
+#endif
+}
+
+void cpu_info_check()
+{
+    int data[4] = { 0 };
+
+    // Add more code here to extract vendor string or what ever is needed
+    __cpuid(data, 0);
+    unsigned int cpuData0 = data[0];
+    unsigned int cpuInfo2 = 0;
+    unsigned int cpuIDFeatures = 0;
+    if (cpuData0 >= 1)
+    {
+        __cpuid(data, 1);
+        cpuInfo2 = data[2];
+        cpuIDFeatures = data[3];
+    }
+
+    // SSE2 support
+    bool m_IsSSE2Supported = (cpuIDFeatures & CPUID_FEATURES_SSE2) != 0;
+
+    // SSE 3.x
+    bool m_IsSSE3Supported = ((cpuInfo2 & (1 << 0)) != 0);
+    bool m_IsSupplementalSSE3Supported = ((cpuInfo2 & (1 << 9)) != 0);
+
+    // SSE 4.x support
+    bool m_IsSSE41Supported = ((cpuInfo2 & (1 << 19)) != 0);
+    bool m_IsSSE42Supported = ((cpuInfo2 & (1 << 20)) != 0);
+
+    // AVX support
+    bool m_IsAVXSupported =
+        ((cpuInfo2 & (1 << 28)) != 0) && // AVX support in CPU
+        ((cpuInfo2 & (1 << 27)) != 0) && // OS support for AVX (XSAVE/XRESTORE on context switches)
+        ((xgetbv_impl() & 6) == 6); // XMM & YMM registers will be preserved on context switches
+
+    if (m_IsAVXSupported)
+    {
+        if (cpuData0 >= 7)
+        {
+            uint32_t regs7[4] = { 0 };
+            cpuid_ex_impl(0x7, 0, regs7);
+            bool m_IsAVX2Supported = ((regs7[1] & (1 << 5)) != 0);
+            bool m_IsAVX512Supported = ((regs7[1] & (1 << 16)) != 0);
+        }
+    }
+
+    bool m_IsFP16CSupported = ((cpuInfo2 & (1 << 29)) != 0);
+    bool m_IsFMASupported = ((cpuInfo2 & (1 << 12)) != 0);
+
+    bool m_IsAdvancedBitManipulationSupported = ((cpuInfo2 & (1 << 23)) != 0);
+}
+
+int main()
+{
+    cpu_info_check();
+}
---
+++ opengl_feature_check/opengl_feature_check.cpp
@@ -0,0 +1,176 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 400;
+const unsigned int SCR_HEIGHT = 300;
+
+static void APIENTRY gl_debug_output(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Test<GLFW+GLAD>", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(gl_debug_output, NULL);
+
+    int profile;
+    glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &profile);
+
+    if (profile & GL_CONTEXT_CORE_PROFILE_BIT) {
+        std::cout << "Core Profile" << std::endl;
+    }
+    else if (profile & GL_CONTEXT_COMPATIBILITY_PROFILE_BIT) {
+        std::cout << "Compatibility Profile" << std::endl;
+    }
+    else {
+        std::cout << "Unknown Profile" << std::endl;
+    }
+
+    auto extensions = glGetString(GL_EXTENSIONS);
+    if (extensions)
+        std::cout << extensions << std::endl;
+
+    std::cout << "error: " << glGetError() << std::endl;
+
+    GLint numExtensions;
+    glGetIntegerv(GL_NUM_EXTENSIONS, &numExtensions);
+
+
+    std::cout << "ext num: " << numExtensions << std::endl;
+
+    for (GLint i = 0; i < numExtensions; i++) {
+        const char* extension = reinterpret_cast<const char*>(glGetStringi(GL_EXTENSIONS, i));
+        std::cout << extension << std::endl;
+    }
+
+    std::cout << "GLAD_GL_KHR_texture_compression_astc_ldr: " << GLAD_GL_KHR_texture_compression_astc_ldr << std::endl;
+
+    GLint supported;
+    glGetInternalformativ(GL_TEXTURE_2D, GL_COMPRESSED_RGBA_ASTC_8x8, GL_INTERNALFORMAT_SUPPORTED, 1, &supported);
+    std::cout << "support GL_COMPRESSED_RGBA_ASTC_8x8 " << supported << std::endl;
+
+    glGetInternalformativ(GL_TEXTURE_CUBE_MAP, GL_COMPRESSED_RGBA8_ETC2_EAC, GL_INTERNALFORMAT_SUPPORTED, 1, &supported);
+    std::cout << "support GL_TEXTURE_CUBE_MAP GL_COMPRESSED_RGBA8_ETC2_EAC " << supported << std::endl;
+
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_COMPRESSED_RGBA_ASTC_8x8, 1024, 1024);
+
+    std::cout << "error: " << glGetError() << std::endl;
+
+    glDeleteTextures(1, &texture);
+
+    const char* version = (const char*)glGetString(GL_VERSION);
+    std::cout << "version: " << version << std::endl;
+
+    // glfw: terminate, clearing all previously allocated GLFWresources.
+    glfwTerminate();
+
+    system("pause");
+    return 0;
+}
+
+static void APIENTRY gl_debug_output(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
+        return;
+    if (severity == GL_DEBUG_SEVERITY_LOW || severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+    {
+        return;
+    }
+
+    std::cout << "\ndebug message(" << id << "):" << message << "\n";
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API:
+        std::cout << "Source: API ";
+        break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        std::cout << "Source: Window System ";
+        break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        std::cout << "Source: Shader Compiler ";
+        break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:
+        std::cout << "Source: Third Party ";
+        break;
+    case GL_DEBUG_SOURCE_APPLICATION:
+        std::cout << "Source: APPLICATION ";
+        break;
+    case GL_DEBUG_SOURCE_OTHER:
+        break;
+    }
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:
+        std::cout << "Type: Error ";
+        break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        std::cout << "Type: Deprecated Behaviour ";
+        break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        std::cout << "Type: Undefined Behaviour ";
+        break;
+    case GL_DEBUG_TYPE_PORTABILITY:
+        std::cout << "Type: Portability ";
+        break;
+    case GL_DEBUG_TYPE_PERFORMANCE:
+        std::cout << "Type: Performance ";
+        break;
+    case GL_DEBUG_TYPE_MARKER:
+        std::cout << "Type: Marker ";
+        break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:
+        std::cout << "Type: Push Group ";
+        break;
+    case GL_DEBUG_TYPE_POP_GROUP:
+        std::cout << "Type: Pop Group ";
+        break;
+    case GL_DEBUG_TYPE_OTHER:
+        std::cout << "Type: Other ";
+        break;
+    }
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:
+        std::cout << "Severity: high";
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        std::cout << "Severity: medium";
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        std::cout << "Severity: low";
+        break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        std::cout << "Severity: notification";
+        break;
+    }
+    std::cout << std::endl;
+}
